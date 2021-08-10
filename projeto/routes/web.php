1729462aaae2eaedf0fed43b36e2262de58c216f
Raynder <raynder4@gmail.com>
2021-08-10 20:39:33 +0000

    Criando novas rotas

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -13,16 +13,10 @@ Route::get("/contato", "ContatoController@principal");
 //O laravel buscara a função mais similar ao que foi requisitado na URL
 //Posso passar varios parametros, porem sempre separados por /, devendo sempre ter uma atenção aos parametros
 
-//Ao colocar ? ao lado direito do parametro e atribuir um default à $ ele fica opcional,
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function(
-        string $nome,
-        int $categoria_id
-    ){
-    echo("
-        Nome: $nome <br>
-        Categoria: $categoria_id<br>
-    ");
-})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
-//Ultilizando o where para condicionar os parametros da rota
+Route::get('/login', function(){echo('teste');});
+
+Route::get('/clientes', function(){echo('teste');});
+
+Route::get('/fornecedores', function(){echo('teste');});
+
+Route::get('/produtos', function(){echo('teste');});
