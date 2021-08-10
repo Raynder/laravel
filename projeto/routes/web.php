178d8e7f2eb979643dc0d4084dccdd69e860d17b
Raynder <raynder4@gmail.com>
2021-08-10 20:54:33 +0000

    saida

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -3,19 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get("/","PrincipalController@principal");
+Route::get("/","PrincipalController@principal")->name('site.index');
 
-Route::get("/sobre-nos", "SobreNosController@principal");
+Route::get("/sobre-nos", "SobreNosController@principal")->name('site.sobrenos');
 
-Route::get("/contato", "ContatoController@principal");
+Route::get("/contato", "ContatoController@principal")->name('site.contato');
 
 //Ao colocar {} depois de uma barra, estamos informando que ser trata de um parametro
 //O laravel buscara a função mais similar ao que foi requisitado na URL
 //Posso passar varios parametros, porem sempre separados por /, devendo sempre ter uma atenção aos parametros
 
 Route::prefix('/app')->group(function(){ 
-    Route::get('/login', function(){echo('teste');});
-    Route::get('/clientes', function(){echo('teste');});
-    Route::get('/fornecedores', function(){echo('teste');});
-    Route::get('/produtos', function(){echo('teste');});
+    Route::get('/login', function(){echo('teste');})->name('app.login');
+    Route::get('/clientes', function(){echo('teste');})->name('app.clientes');
+    Route::get('/fornecedores', function(){echo('teste');})->name('app.fornecedores');
+    Route::get('/produtos', function(){echo('teste');})->name('app.produtos');
 });
