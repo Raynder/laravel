d564fb6ac68a827f0ffa307d7ab452802ef9ea20
Raynder <raynder4@gmail.com>
2021-08-13 16:23:49 +0000

    If Else em views simplificado

--- projeto/resources/views/app/fornecedor/index.blade.php
+++ projeto/resources/views/app/fornecedor/index.blade.php
@@ -1,8 +1,18 @@
 <h1>Fornecedores aqui</h1>
 
-{{-- COMENTARIO --}}
 
 @php
-    echo('teste');
-    //teste
+  
+
+
 @endphp
+
+{{-- @dd($fornecedores) --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Exite mais de 10 fornecedores cadastrados</h3>
+@else
+    <h3>Não existe fornecedores cadastrados</h3>
+@endif
