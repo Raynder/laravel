bfac8b64bc005b9ae157d29273d7ab8a0bbf530d
Raynder <raynder4@gmail.com>
2021-08-13 16:45:39 +0000

    usando isset para tratar erros

--- projeto/app/Http/Controllers/FornecedorController.php
+++ projeto/app/Http/Controllers/FornecedorController.php
@@ -9,7 +9,18 @@ class FornecedorController extends Controller
     public function index(){
         $fornecedores = [
             0 => [
-                'nome' =>'fornecedor 1', 'status' => 'N'
+                'nome' =>'fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00000000000'
+            ],
+            1 => [
+                'nome' =>'fornecedor 2',
+                'status' => 'N',
+                'cnpj' => '00000000000'
+            ],
+            2 => [
+                'nome' =>'fornecedor 3',
+                'status' => 'N'
             ]
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
