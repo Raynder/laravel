ad3280134648d8932e947918054da84248230c69
Raynder Cardoso <raynder4@gmail.com>
2021-08-24 21:23:19 -0300

    old para preenchimento de contatos

--- projeto/resources/views/site/layouts/_components/form_contato.blade.php
+++ projeto/resources/views/site/layouts/_components/form_contato.blade.php
@@ -4,11 +4,11 @@
 </pre>
 <form action="{{route('site.contato')}}" method="post">
     @csrf
-    <input type="text" name="nome" placeholder="Nome" class="{{ $class }}">
+    <input type="text" name="nome" placeholder="Nome" value="{{ old('nome')}}" class="{{ $class }}">
     <br>
-    <input type="text" name="telefone" placeholder="Telefone" class="{{ $class }}">
+    <input type="text" name="telefone" value="{{ old('telefone')}}" placeholder="Telefone" class="{{ $class }}">
     <br>
-    <input type="text" name="email" placeholder="E-mail" class="{{ $class }}">
+    <input type="text" name="email" value="{{ old('email')}}" placeholder="E-mail" class="{{ $class }}">
     <br>
     {{ $slot }}
     <select name="motivo_contato" class="{{ $class }}">
@@ -18,7 +18,13 @@
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $class }}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $class }}">
+    @if(old('mensagem') != '')
+        {{old('mensagem')}}
+    @else
+        Preencha aqui sua mensagem
+    @endif
+    </textarea>
     <br>
     <button type="submit" class="{{ $class }}">ENVIAR</button>
 </form>
