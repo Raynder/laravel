5e162239555aa3ece02c6d92d044f290e18d0cae
Raynder Cardoso <raynder4@gmail.com>
2021-08-24 21:13:29 -0300

    validação de minimo e maximos

--- projeto/app/Http/Controllers/ContatoController.php
+++ projeto/app/Http/Controllers/ContatoController.php
@@ -38,11 +38,11 @@ class ContatoController extends Controller
     public function salvar(Request $request){
 
         $request->validate([
-            'nome' => 'required',
+            'nome' => 'required|min:3|max:40',
             'email' => 'required',
             'telefone' => 'required',
             'motivo_contato' => 'required',
-            'mensagem' => 'required'
+            'mensagem' => 'required|max:2000'
         ]);
 
         //SiteContato::create($request->all());
